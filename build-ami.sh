fa8a2fe271ca58a97ca6ac0c0b95ef40b8a5ec65
Steven Schlansker <sschlansker@opentable.com>
2014-05-07 16:03:46 -0700

    no reason to use EBS anymore

--- build-ami.sh
+++ build-ami.sh
@@ -21,28 +21,6 @@ CURL='curl -fsS'
 echo "AMI tools at $EC2_AMITOOL_HOME"
 echo "Building $RELEASE base image in $ROOT_DIR, named $IMAGE_NAME, uploading to $S3_BUCKET"
 
-function wait_file() {
-  x=0
-  while [ "$x" -lt 100 -a ! -e $1 ]; do
-          x=$((x+1))
-          sleep .1
-  done
-}
-
-if [ -e /dev/xvdz ]; then
-  echo "/dev/xvdz is already assigned"
-  exit 1
-fi
-
-INSTANCE_ID=$($CURL http://169.254.169.254/latest/meta-data/instance-id)
-VOL_ID=$(aws ec2 create-volume --size 10 --availability-zone us-west-2c | jq -r '.VolumeId')
-aws ec2 create-tags --resources $VOL_ID --tags "Key=Name,Value=$IMAGE_NAME"
-aws ec2 attach-volume --volume-id $VOL_ID --instance-id $INSTANCE_ID --device /dev/xvdz
-wait_file /dev/xvdz
-mkfs.ext3 -q -L ec2root /dev/xvdz
-mount /dev/xvdz $ROOT_DIR
-
-
 cd $ROOT_DIR
 debootstrap --include=cloud-init,man-db,manpages-dev,wget,git,git-man,curl,zsh,rsync,screen,lsof,mlocate,nano,ssh,pax,strace,linux-image-virtual,grub,postfix,bsd-mailx,apt-transport-https,ntp,unzip,ruby,kpartx,gdisk,patch $RELEASE . http://us-west-2.ec2.archive.ubuntu.com/ubuntu/
 
@@ -148,23 +126,14 @@ rm tmp/init-setup.sh usr/sbin/policy-rc.d
 cd
 
 umount -l ${ROOT_DIR}{/sys,/proc,/dev}
-sync
 
 [ -e /tmp/$IMAGE_NAME ] && rm -f /tmp/$IMAGE_NAME*
 
 $EC2_AMITOOL_HOME/bin/ec2-bundle-vol -c $EC2_CERT -k $EC2_PRIVATE_KEY -u $AWS_ACCOUNT_ID -r x86_64 -p $IMAGE_NAME -s 10240 -v $ROOT_DIR --fstab $SCRIPT_DIR/config/fstab --no-inherit -B ami=sda,root=/dev/sda1,swap=/dev/sdb,ephemeral0=/dev/sdc,ephemeral1=/dev/sdd --no-filter
 $EC2_AMITOOL_HOME/bin/ec2-upload-bundle -b $S3_BUCKET -a $AWS_ACCESS_KEY -s $AWS_SECRET_KEY --region $EC2_REGION -m /tmp/$IMAGE_NAME.manifest.xml --retry
 AMI=$(aws ec2 register-image --image-location $S3_BUCKET/$IMAGE_NAME.manifest.xml --name $IMAGE_NAME --architecture x86_64 --kernel-id aki-fc8f11cc | jq -r .ImageId)
-echo "AMI is $AMI"
 aws ec2 create-tags --resources $AMI --tags "Key=Name,Value=$IMAGE_NAME" "Key=ot-base-image,Value=ubuntu"
 
-rm /tmp/$IMAGE_NAME*
-
-umount $ROOT_DIR
-rmdir $ROOT_DIR
-
-aws ec2 detach-volume --volume-id $VOL_ID
-sleep 30
-aws ec2 delete-volume --volume-id $VOL_ID
+rm -rf $ROOT_DIR /tmp/$IMAGE_NAME*
 
 echo "Completed: $AMI"
