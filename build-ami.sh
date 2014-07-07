43f5e801dbcc8e3bca5fef66ea2429c0a369f3d1
Steven Schlansker <sschlansker@opentable.com>
2014-07-07 16:50:33 -0700

    AKI per region

--- build-ami.sh
+++ build-ami.sh
@@ -21,8 +21,17 @@ IMAGE_NAME=$2
 RELEASE=trusty
 CURL='curl -fsS'
 
+if [ $EC2_REGION = us-west-1 ]; then
+  AKI="aki-880531cd"
+else if [ $EC2_REGION = us-west2- ]; then
+  AKI="aki-fc8f11cc"
+else
+  echo "Unknown region $EC2_REGION"
+  exit 1
+fi
+
 echo "AMI tools at $EC2_AMITOOL_HOME"
-echo "Building $RELEASE base image in $ROOT_DIR, named $IMAGE_NAME, uploading to $S3_BUCKET"
+echo "Building $RELEASE base image in $ROOT_DIR, kernel $AKI, named $IMAGE_NAME, uploading to $S3_BUCKET"
 
 cd $ROOT_DIR
 debootstrap --include=cloud-init,man-db,manpages-dev,wget,git,git-man,curl,zsh,rsync,screen,lsof,mlocate,nano,ssh,pax,strace,linux-image-virtual,grub,postfix,bsd-mailx,apt-transport-https,ntp,unzip,ruby,kpartx,gdisk,patch $RELEASE . http://us-west-2.ec2.archive.ubuntu.com/ubuntu/
@@ -154,7 +163,7 @@ aws s3 cp /tmp/$IMAGE_NAME.tgz s3://$S3_BUCKET/
 
 $EC2_AMITOOL_HOME/bin/ec2-bundle-vol -c $EC2_CERT -k $EC2_PRIVATE_KEY -u $AWS_ACCOUNT_ID -r x86_64 -p $IMAGE_NAME -s 10240 -v $ROOT_DIR --fstab $SCRIPT_DIR/config/fstab --no-inherit -B ami=sda,root=/dev/sda1,swap=/dev/sdb,ephemeral0=/dev/sdc,ephemeral1=/dev/sdd --no-filter
 $EC2_AMITOOL_HOME/bin/ec2-upload-bundle -b $S3_BUCKET -a $AWS_ACCESS_KEY -s $AWS_SECRET_KEY --region $EC2_REGION -m /tmp/$IMAGE_NAME.manifest.xml --retry
-AMI=$(aws ec2 register-image --image-location $S3_BUCKET/$IMAGE_NAME.manifest.xml --name $IMAGE_NAME --architecture x86_64 --kernel-id aki-fc8f11cc | jq -r .ImageId)
+AMI=$(aws ec2 register-image --image-location $S3_BUCKET/$IMAGE_NAME.manifest.xml --name $IMAGE_NAME --architecture x86_64 --kernel-id $AKI | jq -r .ImageId)
 aws ec2 create-tags --resources $AMI --tags "Key=Name,Value=$IMAGE_NAME" "Key=ot-base-image,Value=ubuntu"
 
 rm -rf $ROOT_DIR /tmp/$IMAGE_NAME*
