a3ad9085f6170df4643eb39c950f5580fb2db9d2
Steven Schlansker <sschlansker@opentable.com>
2014-12-04 10:26:30 -0800

    Use awsbuild docker images instead of a stage2

--- tc-build.sh
+++ tc-build.sh
@@ -2,4 +2,4 @@
 
 set -o errexit -o nounset -o xtrace
 
-docker run --privileged -v $PWD:/otpl-ami-ubuntu -e EC2_REGION=us-west-2 -w /otpl-ami-ubuntu registry.mesos-vpcqa.otenv.com/ot-ubuntu:latest ./tc-build-stage2.sh "$@"
+docker run --privileged -v $PWD:/otpl-ami-ubuntu -e EC2_REGION=us-west-2 -w /otpl-ami-ubuntu registry.mesos-vpcqa.otenv.com/ot-awsbuild:latest ./build-ami "$@"
