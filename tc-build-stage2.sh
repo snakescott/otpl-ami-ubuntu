a19a0d61210bcb56d1fb85c36ecfa9b7f97aa614
Steven Schlansker <sschlansker@opentable.com>
2014-12-03 17:35:32 -0800

    Install pip and awscli

--- tc-build-stage2.sh
+++ tc-build-stage2.sh
@@ -3,7 +3,8 @@
 set -o errexit -o nounset -o xtrace
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y ec2-ami-tools debootstrap
+DEBIAN_FRONTEND=noninteractive apt-get install -y ec2-ami-tools debootstrap python-pip
+pip install awscli
 
 export EC2_AMITOOL_HOME=/usr/lib/ec2-ami-tools
 ./build-ami.sh "$@"
