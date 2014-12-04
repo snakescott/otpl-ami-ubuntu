c38e7013cb489022188617574e5054a1ce3070f4
Steven Schlansker <sschlansker@opentable.com>
2014-12-04 12:09:39 -0800

    Correct build-ami name

--- tc-build.sh
+++ tc-build.sh
@@ -2,4 +2,4 @@
 
 set -o errexit -o nounset -o xtrace
 
-docker run --privileged -v $PWD:/otpl-ami-ubuntu -e EC2_REGION=us-west-2 -w /otpl-ami-ubuntu registry.mesos-vpcqa.otenv.com/ot-awsbuild:latest ./build-ami "$@"
+docker run --privileged -v $PWD:/otpl-ami-ubuntu -e EC2_REGION=us-west-2 -w /otpl-ami-ubuntu registry.mesos-vpcqa.otenv.com/ot-awsbuild:latest ./build-ami.sh "$@"
