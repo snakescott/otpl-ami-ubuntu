a898fe7e455e1f996bbe1bebeb7df89e98088bb9
Steven Schlansker <sschlansker@opentable.com>
2014-12-10 09:13:55 -0800

    build-ami.sh -> build.sh, tc-build moves to TC build template

--- tc-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -o errexit -o nounset -o xtrace
-
-TAG=otpl-ami-ubuntu:$(date +"%Y%m%d%H%M%S")
-
-docker build -t $TAG .
-
-docker run --privileged -e EC2_REGION=us-west-2 -w /otpl-ami-ubuntu $TAG ./build-ami.sh "$@"
