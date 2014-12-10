cc7b0bed40de981f0d05b171927f6b727a0068e4
Steven Schlansker <sschlansker@opentable.com>
2014-12-10 15:53:38 -0800

    This version of upload-bundle doesn't take --region

--- build.sh
+++ build.sh
@@ -159,7 +159,7 @@ tar -cz -C $ROOT_DIR -f $IMAGE_NAME.tgz .
 aws s3 cp $IMAGE_NAME.tgz s3://$S3_BUCKET/
 
 ec2-bundle-vol -c $EC2_CERT -k $EC2_PRIVATE_KEY -u $AWS_ACCOUNT_ID -r x86_64 -p $IMAGE_NAME -s 10240 -v $ROOT_DIR --fstab $SCRIPT_DIR/config/fstab --no-inherit -B ami=sda,root=/dev/sda1,swap=/dev/sdb,ephemeral0=/dev/sdc,ephemeral1=/dev/sdd --no-filter
-ec2-upload-bundle -b $S3_BUCKET -a $AWS_ACCESS_KEY -s $AWS_SECRET_KEY --region $EC2_REGION -m /tmp/$IMAGE_NAME.manifest.xml --retry
+ec2-upload-bundle -b $S3_BUCKET -a $AWS_ACCESS_KEY -s $AWS_SECRET_KEY -m /tmp/$IMAGE_NAME.manifest.xml --retry
 AMI=$(aws ec2 register-image --image-location $S3_BUCKET/$IMAGE_NAME.manifest.xml --name $IMAGE_NAME --architecture x86_64 --kernel-id $AKI | jq -r .ImageId)
 aws ec2 create-tags --resources $AMI --tags "Key=Name,Value=$IMAGE_NAME" "Key=ot-base-image,Value=ubuntu"
 
