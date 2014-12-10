cda3e6a7ae5532ea358d17c73856940f955767ab
Steven Schlansker <sschlansker@opentable.com>
2014-12-10 14:34:22 -0800

    docker.io -> docker.com

--- build.sh
+++ build.sh
@@ -106,7 +106,7 @@ cloud-init single -n cc_apt_configure
 
 # install docker
 
-echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 apt-get update
 
