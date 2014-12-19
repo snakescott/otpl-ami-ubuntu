e1b3a9294d5931493d6db7e573bd6b16e3d3f6a8
Steven Schlansker <sschlansker@opentable.com>
2014-12-19 09:24:21 -0800

    If there's only one drive, don't make it a RAID

--- extras/init-btrfs.sh
+++ extras/init-btrfs.sh
@@ -4,10 +4,19 @@ set -o errexit -o xtrace -o nounset
 # Check if we already did this bit
 lsblk -f | grep btrfs && exit 0
 
+seen_first=0
+raid_args=""
+
 disks=""
 for mapping in $(curl -fLS http://169.254.169.254/latest/meta-data/block-device-mapping | grep ephemeral)
 do
   newdisk="$(curl -fLS http://169.254.169.254/latest/meta-data/block-device-mapping/$mapping | sed -E 's#^sd#/dev/xvd#')"
-  [ -e $newdisk ] && disks="$disks $newdisk"
+  if [ -e $newdisk ]; then
+    disks="$disks $newdisk"
+    if [ $seen_first -eq 1 ]; then
+      raid_args="-m raid0 -d raid0"
+    fi
+    seen_first=1
+  fi
 done
-mkfs.btrfs -f -m raid0 -d raid0 $disks
+mkfs.btrfs -f $raid_args $disks
