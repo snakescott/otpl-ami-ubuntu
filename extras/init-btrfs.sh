63d84d9c0322a9501247ccb12984ea23edee1241
Steven Schlansker <sschlansker@opentable.com>
2014-12-18 16:36:55 -0800

    Ensure devices actually exist before trying to use them

--- extras/init-btrfs.sh
+++ extras/init-btrfs.sh
@@ -7,6 +7,7 @@ lsblk -f | grep btrfs && exit 0
 disks=""
 for mapping in $(curl -fLS http://169.254.169.254/latest/meta-data/block-device-mapping | grep ephemeral)
 do
-  disks="$disks $(curl -fLS http://169.254.169.254/latest/meta-data/block-device-mapping/$mapping | sed -E 's#^sd#/dev/xvd#')"
+  newdisk="$(curl -fLS http://169.254.169.254/latest/meta-data/block-device-mapping/$mapping | sed -E 's#^sd#/dev/xvd#')"
+  [ -e $newdisk ] && disks="$disks $newdisk"
 done
 mkfs.btrfs -f -m raid0 -d raid0 $disks
